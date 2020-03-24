7b715aea7a144823d8f3e02822057d8855fc42b5
nahid <nahidtop@gmail.com>
2020-03-24 19:34:15 +0100

    added frontend

---
+++ build/precache-manifest.de59cb81bc07df7355cc96f5773ba5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e486398109c7b0b0d5a778486d456927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bdc047859bf8aebfb08",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "60bc4ca54bf3594ed277",
+    "url": "/static/js/2.1d112ada.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.1d112ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdc047859bf8aebfb08",
+    "url": "/static/js/main.7fce2bfe.chunk.js"
+  },
+  {
+    "revision": "0db0d190caf16c601b6e",
+    "url": "/static/js/runtime-main.c7d3f649.js"
+  }
+]);
