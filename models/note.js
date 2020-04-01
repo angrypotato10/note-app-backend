c68b11db38b2dee28832dd9e4b704bcee63ba2e8
nahid <nahidtop@gmail.com>
2020-04-01 15:35:52 +0200

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,21 +1,8 @@
-const mongoose = require('mongoose');
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const mongoose = require('mongoose')
 
-mongoose.set('useFindAndModify', false);
-
-require('dotenv').config();
-
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB', error.message);
-  });
+mongoose.set('useFindAndModify', false)
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -29,14 +16,15 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
+})
 
+// Used for formatting of JSON output, _id to id and removing __v
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
